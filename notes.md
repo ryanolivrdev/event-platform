fade1a7bfd2e77f610b047c3c2d1592bef35f33c
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-06-21 13:57:36 -0300

    First Commit

---
+++ notes.md
@@ -0,0 +1,10 @@
+  CMS = Content Management System
+    
+    - Wordpress | Tras Tanto o painel de ADMIN tanto quanto a parte visual do front-ent (temas)
+
+  Headless CMS (GraphCMS) | Painel de ADMIN (dados fornecidos atraves de uma API REST ou GraphQL)
+    - React que consome essa API do CMS
+    
+    - query | mutation
+      - query = Buscar dados.
+      - mutation = criar, alterar ou deletar dados 
---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { gql, useQuery } from "@apollo/client"
+
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+interface Lessons { 
+  id: string
+  title: string;
+}
+
+
+function App() {
+  const { data } = useQuery<{ lessons: Lessons[] }>(GET_LESSONS_QUERY);
+
+  console.log(data);
+
+  return (
+    <ul>
+      {data?.lessons.map(lesson => { 
+      return <li key={lesson.id}>{lesson.title}</li>})}
+    </ul>
+  )
+}
+
+export default App
