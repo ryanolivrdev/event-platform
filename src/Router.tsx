27186151bad79e9c83b0099f9e2be27b65e9d6d6
Ryan Oliveira <jryancoelho79@hotmail.com>
2022-06-24 03:55:41 -0300

    Home page add, registration via GraphQL

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { Event } from "./pages/Event";
+import { Subscription } from "./pages/Subscribe";
 
 export function Router(){
   return ( 
     <Routes>
-      <Route path="/" element={<h1>Hello World! HOME</h1>} />
+      <Route path="/" element={<Subscription />} />
       <Route path="/event" element={<Event />} />
       <Route path="/event/lesson/:slug" element={<Event />} />
     </Routes>
--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4od8x711yon01z29un3g1pw/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+  },
   cache: new InMemoryCache()
 });
